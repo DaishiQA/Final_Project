2ef0a07de9a513407bbf1bf38af819f5fbc6e5d0
DaishiQA <torpp32@gmail.com>
2026-08-28 13:01:27 +0300

    hotfix

--- test_get_order_by_track.py
+++ test_get_order_by_track.py
@@ -3,15 +3,18 @@ import configuration
 import data
 import requests
 
+# POST-запрос на создание заказа
 def create_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
         json=body,
         headers=data.headers)
 
+# GET-запрос на получение информации о заказе по трек-номеру
 def get_order_by_track(track):
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK_PATH + str(track),
         headers=data.headers)
 
+# Запуск автотеста на проверку заказа по номеру трека и проверки получения статуса кода 200
 def test_get_order_info_by_track():
     track = create_new_order(data.order_body).json()['track']
     response = get_order_by_track(track)
